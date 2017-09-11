c7e937e44d7c7abf70dd71d2720fe2adfe0bbe4c
Uladzislau Sipaila <Uladzislau_Sipaila@epam.com>
2017-09-11 19:39:32 +0300

    Task-1: Codefixes

--- index.js
+++
@@ -1,6 +0,0 @@
-const config = require('./config/config.json');
-const models = require('./models');
-
-console.log(config.name);
-new models.Product();
-new models.User();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import config from '../config/config.json';
+import {
+    Product,
+    User
+} from './models';
+
+console.log(config.name);
+new Product();
+new User();
